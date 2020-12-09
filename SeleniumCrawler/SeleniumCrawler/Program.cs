ee0dfe5fdc066ac42b96700f9d03a62f37a7e22e
choihyunill <kslim1025@naver.com>
2020-12-09 16:58:26 +0900

    셀레니움 크롤러 처리

--- SeleniumCrawler/SeleniumCrawler/Program.cs
+++ SeleniumCrawler/SeleniumCrawler/Program.cs
@@ -1,24 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
 
 namespace SeleniumCrawler
 {
-   
-    class Program
+    class Program: Attribute
     {
+        static public int Field1 = 0;
+
         static void Main(string[] args)
         {
-
+            /// <summary>
+            ///    Environment.CurrentDirectory 부분이 공백일 경우는 Path 문제가 발생할 수있음
+            /// </summary>
+            using (IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory))
+            {
+                driver.Url = "https://www.naver.com/";
+ 
+                IWebElement q = driver.FindElement(By.Id("query"));
+                q.Clear();
+                q.SendKeys("오늘 날씨");
+                driver.FindElement(By.Id("search_btn")).Click();
+            }
         }
     }
-  
+    
 }
