92d7209871ed5d0ad367dfd6b193f772916e4476
choihyunill <kslim1025@naver.com>
2020-12-09 17:23:04 +0900

    Abort를 사용한 스레드 컨트롤

--- ThreadController/ThreadController/Program.cs
+++ ThreadController/ThreadController/Program.cs
@@ -24,18 +24,37 @@ namespace ThreadController
         ///       </code>
         ///    </example>
         /// </summary>
+        String Commander;
+        Thread _thread;
+        Thread _thread2;
         public Program()
         {
-            new Thread(new ThreadStart(Thread_T1)).Start();
-            new Thread(new ThreadStart(Thread_T2)).Start();
+            _thread = new Thread(new ThreadStart(Thread_T1));
+            _thread.Start();
+            _thread2 = new Thread(new ThreadStart(Thread_T2));
+            _thread2.Start();
+            while (true)
+            {
+                //입력 커맨더를 기다린다.
+                Commander = Console.ReadLine();
+                if (Commander == "exit")
+                {
+                    //스레드를 종료한다.
+                    _thread.Abort();
+                    break;
+                }
+            }
+            WriteLine("");
         }
-
+        //쓰레드 실행 함수
         public void Thread_T1()
         {
             int i = 0;
+            //무한 반복문
             while (true)
             {
-                WriteLine("Thread_T1 i = {0}", i++);
+                WriteLine("Thread_T1 j = {0}", i++);
+                //1초 단위로 루프가 돈다.
                 Thread.Sleep(1000);
             }
         }
