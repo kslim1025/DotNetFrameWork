be8e3d3c4baa39c20e80f448b1369a467e36c2d4
choihyunill <kslim1025@naver.com>
2020-12-09 17:13:12 +0900

    싱글 쓰레드 생성 및 활성 과정 검증

---
+++ ThreadController/ThreadController/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+/// <summary>
+///    Thread_T1(), Thread_T2()를 생성해서 
+///    Program 메소드에서 스레드 실행
+/// </summary>
+namespace ThreadController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+        }
+
+        /// <summary>
+        ///    Program 메소드에서 스레드 실행부분
+        ///    <example>
+        ///       <code>
+        ///         new Thread(new ThreadStart(Thread_T1)).Start();
+        ///         new Thread(new ThreadStart(Thread_T2)).Start();
+        ///       </code>
+        ///    </example>
+        /// </summary>
+        public Program()
+        {
+            new Thread(new ThreadStart(Thread_T1)).Start();
+            new Thread(new ThreadStart(Thread_T2)).Start();
+        }
+
+        public void Thread_T1()
+        {
+            int i = 0;
+            while (true)
+            {
+                WriteLine("Thread_T1 i = {0}", i++);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Thread_T2()
+        {
+            int j = 0;
+            while (true)
+            {
+                WriteLine("Thread_T2 j = {0}", j++);
+                Thread.Sleep(2500);
+            }
+        }
+
+    }
+}
